4e810590a0bddaca189f67d7cf6eaa9b2cc95a71
aslayclevisson <55596127+aslayclevisson@users.noreply.github.com>
2020-11-06 05:55:28 -0300

    Add files via upload

---
+++ dijkstra_implementacao_resultado.py
@@ -0,0 +1,106 @@
+from cria_matriz import lista_matriz
+import leitura_arquivo
+import sys
+
+class Dijkstra:
+    def __init__(self, entrada):# a entrada conta com um dicionário com os todos_vertices e um grafo mostrado por lista de matriz
+        self.instancia = lista_matriz(entrada) # puxa uma classe
+        self.todos_vertices = 0 # onde vai ser armazenado todos os vertices da matriz por chaves de 0 a N
+        self.todos_vertices_inversos = {} # esse dicionário vai usar todos os vertices como chave para os indices em que cada um se encontra
+        self.lista_matriz_do_grafo = 0 # armazena uma lista de listas
+        self.tamanho = 0 # pega a quantidade de vertices
+        self.distancia = [] #Armazena as distâncias entre os vertices
+
+    
+ ###################################################################################################
+    def comecar_e_terminar(self, inicio=None, ponto_final=None):
+        try:
+            print(f'Quero ir do ponto {inicio} até o ponto {ponto_final}.')
+
+            if inicio != None and ponto_final != None:
+                if inicio != ponto_final:
+                    #A variável abaixo, evoca uma função que retorna os indices do ponto inicial e final trocados
+                    #Basicamente, joga o Elemento Inicial pro indice 0 e o Elemento na posição 0, para o lugar onde antes tava o inicial
+                    #O mesmo acontece com Elemento Final                    
+                    self.todos_vertices = self.instancia.onde_comeca_e_onde_termina(inicio,ponto_final)
+
+                    #monta um dicionário inverso, com os dados de todos os vertices inversos, ou seja, passa passa a ser valor e valor a chave
+                    for i, j in self.todos_vertices.items():
+                        self.todos_vertices_inversos[j] = i
+
+                    self.tamanho = len(self.todos_vertices)
+                    self.lista_matriz_do_grafo = self.instancia.pegar_arestas()
+
+                    self.dijkstra()
+                else:
+                    print(f'A distância entre {inicio} e {ponto_final} é igual a 0.')
+        except:
+            print('Um ou mais dos elementos não foram inseridos no grafo.')
+
+###################################################################################################
+    def encontrar_menor_distancia(self, distancia, visitados):
+        try:
+            minimo = sys.maxsize# adota um valor máximo
+            menor_ind = 0
+            distancia = distancia
+
+            #Dá a distância mínima entre um vertice e o outro e retorna seu indice
+            for vertice in range(self.tamanho):
+                if distancia[vertice] < minimo and visitados[vertice] == False:
+                    minimo = distancia[vertice]
+                    menor_ind = vertice
+         
+            return menor_ind
+
+        except:
+            print('erro')
+
+###################################################################################################   
+    def dijkstra(self):
+        self.distancia = [sys.maxsize] * self.tamanho# cria uma lista com valores máximos para serem substituido por mínimos de acordo com a quantidade de vertices
+        visitados = [False] * self.tamanho# cria uma lista para substituir o False pelo True, quando algum vertice esteja sendo visitado
+        self.distancia[0] = 0
+        vertices = 0
+        encontrou_ponto_final = False
+
+
+        for _ in range(self.tamanho):
+            menor_indice = self.encontrar_menor_distancia(self.distancia, visitados)
+            if visitados[menor_indice] == True:
+                break
+
+            visitados[menor_indice] = True  
+
+            if self.todos_vertices[menor_indice] != self.todos_vertices[len(self.todos_vertices)-1]:               
+                #aqui vai ser verificado os adjacentes possíveis de visitar a partir de um vertice visitado
+                for vertice in range(self.tamanho):                
+                    #verifica se a coordenada se é maior que 0 e substitui os valores máximos em self.distância por novos valores reais de distância entre os vertices
+                    if self.lista_matriz_do_grafo[menor_indice][vertice] > 0 and visitados[vertice] == False and self.distancia[vertice] > self.distancia[menor_indice] + self.lista_matriz_do_grafo[menor_indice][vertice]:
+                        self.distancia[vertice] = self.distancia[menor_indice] + self.lista_matriz_do_grafo[menor_indice][vertice]# essa última parte é a soma dos valores das arestas
+
+                    vertices = vertice
+
+            #Verifica se chegou no ponto final
+            elif self.todos_vertices[menor_indice] == self.todos_vertices[len(self.todos_vertices)-1]:
+                encontrou_ponto_final = True
+                print('='*30)
+                print(f'O visitado {self.todos_vertices[menor_indice]}, é o ponto final')
+                print("Estou em --> ", self.todos_vertices[vertices])
+                print(f'A menor soma do ponto {self.todos_vertices[0]} até onde estou -->  {self.distancia[menor_indice]}')
+                print("Soma da distância entre onde estava e onde estou é --> ", self.distancia[vertices])
+                print()
+                print('='*30)
+                break#Qualquer coisa, deixa ir
+
+        #Verifica se algum caminho não pode ser visitado a partir do inicio
+        if encontrou_ponto_final == False:
+            print('Não há caminho para esse ponto.')
+
+arquivo = leitura_arquivo.arquivo()
+dijkstra = Dijkstra(arquivo)
+dijkstra.comecar_e_terminar('1','65')
+
+
+
+
+
