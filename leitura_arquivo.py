e5b9dad7066ecdcf6221dab7121739d766015b59
aslayclevisson <55596127+aslayclevisson@users.noreply.github.com>
2020-11-17 10:12:23 -0300

    Update leitura_arquivo.py

--- leitura_arquivo.py
+++ leitura_arquivo.py
@@ -1,35 +1,29 @@
-import random
 
 def arquivo():
-    arquivo = open("C:\\Users\\Aslay l Cynthia\\Documents\\ProjetoAlgoritmos\\ca-netscience.txt", "r") #obs isso está de acordo com o meu pc
+    arquivo = open("directory\\file.txt", "r")
     linhas = arquivo.readlines()
     linhas.append('\n')
 
-
-
     listaArestas = []
-    #grafo = {}
     contador = 0
     index = ""
 
-    for x in linhas:       
-        for y in x:
-            if y != " " and y != "\n":
-                index += y
+    for v in linhas:       
+        for elem in v:
+            if elem != " " and elem != "\n":
+                index += elem
             
-            if y == " ":
+            if elem == " ":
                 listaArestas.append([(index)])
                 index = ""
 
-            if y == "\n":
+            if v == "\n":
                 listaArestas[contador].append((index))                
                 index = ""
                 contador +=1
 
-
-
-    for x in listaArestas:
-        x.append(1)
+    for lista in listaArestas: 
+        lista.append(1)
 
     #print(listaArestas)
     return listaArestas
